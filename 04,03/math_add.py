7e5416570e4c4befd0925b3f841adb49c6b8f88b
Aleksandr <bagaevs754@gmail.com>
2026-03-04 16:10:01 +0800

    update py files

--- 04,03/math_add.py
+++ 04,03/math_add.py
@@ -1,4 +1,13 @@
 def add(a, b):
     return a+b
 def add_with_bug(a,b):
-    return a*b
+    return a+b
+def add_something(a, b):
+    if a is None or b is None:
+        return 0
+    if isinstance(a, str) or isinstance(b, str):
+        return str(a) + str(b)
+    return a + b
+
+def calculate_tax(income):
+    return income * 0.15
